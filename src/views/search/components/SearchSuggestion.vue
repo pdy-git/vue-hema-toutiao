720b1ebccd7e9b38ad1d6f8dae881e20db166320
pdy-gitee <pdy_1214@163.com>
2022-08-05 16:20:33 +0800

    完成了搜索

---
+++ src/views/search/components/SearchSuggestion.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <van-cell
+      v-for="(item, index) in highlightSuggestions"
+      :key="index"
+      icon="search"
+    >
+      <template #title>
+        <span v-html="item"></span>
+      </template>
+    </van-cell>
+  </div>
+</template>
+
+<script>
+import { getSearchSuggetionsAPI } from '@/api/search'
+import { debounce } from 'lodash'
+export default {
+  data() {
+    return {
+      suggestions: []
+    }
+  },
+  props: {
+    keywords: {
+      type: String,
+      require: true
+    }
+  },
+  methods: {
+    //   getSearchSuggetion() {
+    //     clearTimeout(this.id)
+    //     this.id = setTimeout(async () => {
+    //       try {
+    //         const { data } = await getSearchSuggetionsAPI(this.keywords)
+    //         this.suggestions = data.data.options
+    //         console.log(data)
+    //       } catch (error) {
+    //         this.$toast.fail('获取建议失败')
+    //       }
+    //     }, 1000)
+    //   }
+    // },
+    getSearchSuggetion: debounce(async function () {
+      try {
+        const { data } = await getSearchSuggetionsAPI(this.keywords)
+        this.suggestions = data.data.options.filter(Boolean)
+        console.log(data)
+      } catch (error) {
+        this.$toast.fail('获取建议失败')
+      }
+    }, 1000)
+  },
+  watch: {
+    keywords: {
+      immediate: true,
+      handler() {
+        this.getSearchSuggetion()
+      }
+    }
+  },
+  computed: {
+    highlightSuggestions() {
+      const reg = new RegExp(this.keywords, 'ig')
+
+      return this.suggestions.map((item) =>
+        item.replace(reg, (match) => `<span style="color:red">${match}</span>`)
+      )
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ src/views/search/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <!-- 搜索框 -->
+    <form action="/">
+      <van-search
+        v-model.trim="keyWords"
+        background="#3296fa"
+        placeholder="请输入搜索关键词"
+        show-action
+        class="search"
+        @search="onSearch"
+        @focus="onSearchFocus"
+      />
+    </form>
+    <!-- 搜索历史 -->
+    <component :is="componentName" :keywords="keyWords"></component>
+  </div>
+</template>
+
+<script>
+import SearchHistory from './components/SearchHistory.vue'
+import SearchResul from './components/SearchResult.vue'
+import SearchSuggestion from './components/SearchSuggestion.vue'
+export default {
+  name: 'my-search',
+  data() {
+    return {
+      keyWords: '',
+      isShowSearchResult: false
+    }
+  },
+  components: {
+    SearchHistory,
+    SearchResul,
+    SearchSuggestion
+  },
+  computed: {
+    componentName() {
+      if (this.keyWords === '') {
+        return SearchHistory
+      }
+
+      if (this.isShowSearchResult) {
+        return SearchResul
+      }
+
+      return SearchSuggestion
+    }
+  },
+  methods: {
+    onSearch() {
+      this.isShowSearchResult = true
+      console.log('正在搜索')
+    },
+    onSearchFocus() {
+      this.isShowSearchResult = false
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.search {
+  [role='button'] {
+    color: #fff;
+  }
+}
+</style>
